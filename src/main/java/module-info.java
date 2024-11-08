8f80e071450dbb7c1414ef7387b24428e5732add
Ron Webb <ron@ronella.xyz>
2024-11-08 20:58:51 +1300

    Update the module dependencies for log4j2.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@
  * @since 1.0.0
  */
 open module xyz.ronella.template.api {
+
     requires static lombok;
     requires static java.net.http;
 
@@ -15,7 +16,9 @@ open module xyz.ronella.template.api {
     requires xyz.ronella.logging.logger.plus;
 
     requires com.google.guice;
-    requires org.slf4j;
+
     requires org.apache.logging.log4j;
+    requires org.apache.logging.log4j.core;
+    requires org.apache.logging.log4j.slf4j2.impl;
 
 }
